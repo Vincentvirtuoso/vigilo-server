5eadbf314943bf93385927da371168f0174d2e81
Vincentvirtuoso <vincentvirtuoso66@gmail.com>
2025-09-11 22:27:58 +0100

    server:auth_controller_fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const registerUser = async (req, res, next) => {
       !department ||
       !faculty ||
       (role === 'student' && !level) ||
-      !matricNumber
+      (role === student && !matricNumber)
     )
       throw createHttpError(400, 'All fields are required');
 
