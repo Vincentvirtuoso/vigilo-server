f548d5c8e5f6fd10221bb1ccd25790d135b98492
Felix Vincent <vincentvirtuoso66@gmail.com>
2026-01-03 19:41:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ async function startServer() {
     const server = http.createServer(app);
     const io = new Server(server, {
       cors: {
-        origin: ['http://localhost:5173', 'https://vigilo-app.onrender.com'],
+        origin: ['http://localhost:5173', 'https://vigilo-app.onrender.com', 'https://vigilo-faj6.onrender.com'],
         credentials: true,
       },
     });
