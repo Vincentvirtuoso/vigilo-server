93e102e67efdd0f61268e48142632d68f245b927
Felix Vincent <vincentvirtuoso66@gmail.com>
2026-01-09 21:23:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ import http from 'http';
 import { Server } from 'socket.io';
 import app from './src/app.js';
 
-dotenv.config({ path: './.env' });
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
